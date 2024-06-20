d62ba49f0649e324cf5bc05852588d272cbd35af
nsrdr <hiteshahire4887@gmail.com>
2024-06-21 02:23:09 +0530

    new-portfolio

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <div class="container mx-auto p-4">
+      <h1 class="text-4xl bg-blue-600 font-bold text-center">My Portfolio</h1>
+      <h1>This is an about page</h1>
+    </div><br>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
